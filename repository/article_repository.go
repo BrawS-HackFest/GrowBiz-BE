1c2b705225b816448064be31cb73f0a271fd07f3
izra4 <aryaizra2@gmail.com>
2024-01-17 11:07:48 +0700

    add: article repository

---
+++ repository/article_repository.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"HackFest/models"
+	"gorm.io/gorm"
+	"log"
+)
+
+type ArticleRepository interface {
+	Create(article models.Article) error
+	FindAll() ([]models.Article, error)
+	FindByID(id uint) (models.Article, error)
+}
+
+type articleRepository struct {
+	db *gorm.DB
+}
+
+func NewArticleRepository(db *gorm.DB) ArticleRepository {
+	return &articleRepository{db}
+}
+
+func (a *articleRepository) Create(article models.Article) error {
+	if err := a.db.Create(&article).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *articleRepository) FindAll() ([]models.Article, error) {
+	var data []models.Article
+	err := a.db.Find(&data).Error
+	if err != nil {
+		log.Println("Error FindAll article: ", err)
+		return nil, err
+	}
+	return data, nil
+}
+
+func (a *articleRepository) FindByID(id uint) (models.Article, error) {
+	var data models.Article
+	err := a.db.First(&data, id).Error
+	if err != nil {
+		log.Println("Error FindByID article: ", err)
+		return data, err
+	}
+	return data, nil
+}
